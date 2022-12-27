62fa91ef06ad06259af3bd908fb612876327c6f1
Liudmyla007 <ldatsenko3@gmail.com>
2022-12-27 17:52:36 +0200

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,2 +1,24 @@
-const a = 10;
-console.log(a);
+const chameleonBody = document.querySelector("body");
+const BtnStart = document.querySelector('button[data-start]');
+const BtnStop = document.querySelector('button[data-stop]');
+
+let timerId = null;
+
+function getRandomHexColor() {
+  let newColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  chameleonBody.style.backgroundColor = newColor;
+};
+
+BtnStart.addEventListener("click", () => {
+  timerId = setInterval(() => {
+    getRandomHexColor();
+      console.log("Start");
+  }, 1000);
+    
+});
+
+BtnStop.addEventListener("click", () => {
+    clearInterval(timerId); 
+    console.log("finish"); 
+});
+
