2509de0ac7f42c62b046cc53db68e58aa38d3c1c
Liudmyla007 <ldatsenko3@gmail.com>
2022-12-29 18:28:25 +0200

    task2 finish

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -16,6 +16,14 @@ const date = new Date();
 
 let timerId = null;
 
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+};
+
+function addLeadingZeroYear(value) {
+  return String(value).padStart(3, '0');
+};
+
 function convertMs(ms) {
   // Number of milliseconds per unit of time
   const second = 1000;
@@ -24,13 +32,13 @@ function convertMs(ms) {
   const day = hour * 24;
 
   // Remaining days
-  const days = Math.floor(ms / day);
+  const days = addLeadingZeroYear(Math.floor(ms / day));
   // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
   // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
   // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
 
   timerDays.textContent = days;
   timerHours.textContent = hours;
